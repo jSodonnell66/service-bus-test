1951e297e68ade61217e268f83551944e8276074
Jack O'Donnell <jack.odonnell@z.co.nz>
2023-11-20 13:54:24 +1300

    Retreive messages from the cue

---
+++ QueueReceiver/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+
+// the client that owns the connection and can be used to create senders and receivers
+ServiceBusClient client;
+
+// the processor that reads and processes messages from the queue
+ServiceBusProcessor processor;
